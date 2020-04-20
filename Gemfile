4ef703f1c8369356f113889ef3c37c860375589c
P0rtk3y <llama.p0rtk3y@gmail.com>
2020-04-20 11:37:30 -0700

    adds audiocard model

--- Gemfile
+++ Gemfile
@@ -38,7 +38,7 @@ gem 'bootsnap', '>= 1.1.0', require: false
 gem 'rack-cors'
 
 #serializer
-get 'fast_jsonapi'
+gem 'fast_jsonapi'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_19_235944) do
+ActiveRecord::Schema.define(version: 2020_04_20_173722) do
+
+  create_table "audiocards", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "category"
+    t.string "tags"
+    t.string "soundFile"
+    t.string "soundster"
+    t.string "image"
+    t.string "mood"
+    t.boolean "privateCard"
+    t.boolean "favorite"
+    t.datetime "created_at", null: false
+    t.index ["user_id"], name: "index_audiocards_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "firstname"
