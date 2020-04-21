4e56c679ea71f41545174d17fb14912e2c62fcf6
P0rtk3y <llama.p0rtk3y@gmail.com>
2020-04-20 23:05:05 -0700

    add sessions for user login

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,6 @@
+class Api::V1::SessionsController < ApplicationController
+    def create
+    end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get "/api/v1/get_current_user", to: "api/v1/sessions#get_current_user"
+  get "/api/v1/login", to: "sessions#create"
 
   namespace :api do 
     namespace :v1 do
